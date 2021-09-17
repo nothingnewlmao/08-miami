4c897fb49c208b5a25ba1e59c1bfd9246aa79681
a18245107 <AOSherstnev@sberbank.ru>
2021-09-17 21:33:40 +0300

    new component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,16 @@
 import React, { useState } from "react";
-import { Button } from "../../ui/components/Button";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "../../ui/global";
 import { themes } from "../../ui/themes";
-import { Input } from "../../ui/components/Input";
+import { Example } from "./Example/Example";
 
 const App = () => {
     const [selectedTheme, setSelectedTheme] = useState(themes.light);
 
-    const changeThemeToLight = () => {
-        setSelectedTheme(themes.light);
-    };
-
-    const changeThemeToBlue = () => {
-        setSelectedTheme(themes.sea);
-    };
-
     return (
         <ThemeProvider theme={selectedTheme}>
             <GlobalStyles />
-            <Button size="l" view="primary" onClick={changeThemeToLight}>
-                Светлая тема
-            </Button>
-            <Button size="s" view="warning" onClick={changeThemeToBlue}>
-                Голубая тема
-            </Button>
-            <Input placeholder="hello" />
+            <Example setTheme={setSelectedTheme} />
         </ThemeProvider>
     );
 };
---
+++ src/components/Example/Example.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+import { Button } from "../../../ui/components/Button";
+import { themes } from "../../../ui/themes";
+import { Input } from "../../../ui/components/Input";
+
+export const Example: FC<{ setTheme: Function }> = ({ setTheme }) => {
+    const changeThemeToLight = () => {
+        setTheme(themes.light);
+    };
+
+    const changeThemeToBlue = () => {
+        setTheme(themes.sea);
+    };
+
+    return (
+        <>
+            <Button size="l" view="primary" onClick={changeThemeToLight}>
+                Светлая тема
+            </Button>
+            <Button size="s" view="warning" onClick={changeThemeToBlue}>
+                Голубая тема
+            </Button>
+            <Input placeholder="hello" />
+        </>
+    );
+};
