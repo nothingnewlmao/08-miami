e1291a33f900c4cea6c278f7f831dc64ba47c211
a18245107 <AOSherstnev@sberbank.ru>
2021-09-23 15:22:34 +0300

    add sort

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -2,35 +2,7 @@ import React, { FC } from 'react';
 import * as Styled from './styled';
 import { TableHead } from './TableHead/TableHead';
 import { TableBody } from './TableBody/TableBody';
-import { ILeadersProps } from './types';
-
-const fakeElements: ILeadersProps[] = [
-    {
-        id: 1,
-        name: 'Алла',
-        points: 10,
-    },
-    {
-        id: 2,
-        name: 'Боря',
-        points: 5,
-    },
-    {
-        id: 3,
-        name: 'Сева',
-        points: 15,
-    },
-    {
-        id: 4,
-        name: 'Степа',
-        points: 20,
-    },
-    {
-        id: 5,
-        name: 'Саня',
-        points: 3,
-    },
-];
+import { fakeElements } from './fakeData';
 
 export const Leaderboard: FC = () => (
     <Styled.Wrapper>
--- src/components/Leaderboard/TableBody/TableBody.tsx
+++ src/components/Leaderboard/TableBody/TableBody.tsx
@@ -2,14 +2,17 @@ import React, { FC } from 'react';
 import * as Styled from './styled';
 import { ITableBodyProps } from './types';
 
-export const TableBody: FC<ITableBodyProps> = ({ elements }) => (
-    <tbody>
-        {elements.map(el => (
-            <Styled.BodyTr key={el.id}>
-                <Styled.BodyTd>{el.id}</Styled.BodyTd>
-                <Styled.BodyTd>{el.name}</Styled.BodyTd>
-                <Styled.BodyTd>{el.points}</Styled.BodyTd>
-            </Styled.BodyTr>
-        ))}
-    </tbody>
-);
+export const TableBody: FC<ITableBodyProps> = ({ elements }) => {
+    const sorted = elements.sort((a, b) => b.points - a.points).slice(0, 5);
+    return (
+        <tbody>
+            {sorted.map((el, index) => (
+                <Styled.BodyTr key={el.id}>
+                    <Styled.BodyTd>{index + 1}</Styled.BodyTd>
+                    <Styled.BodyTd>{el.name}</Styled.BodyTd>
+                    <Styled.BodyTd>{el.points}</Styled.BodyTd>
+                </Styled.BodyTr>
+            ))}
+        </tbody>
+    );
+};
