95a84966ca02cf480606d11be0c92f5216e62f13
oduvankenobi <43110071+oduvankenobi@users.noreply.github.com>
2021-09-23 12:29:40 +0300

    Feature/miami-11 добавление роутинга (#4)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { GlobalStyles } from '../../ui/global';
 import { themes } from '../../ui/themes';
 import { Example } from './Example/Example';
@@ -8,10 +9,18 @@ const App: FC = () => {
     const [selectedTheme, setSelectedTheme] = useState(themes.light);
 
     return (
-        <ThemeProvider theme={selectedTheme}>
-            <GlobalStyles />
-            <Example setTheme={setSelectedTheme} />
-        </ThemeProvider>
+        <Router>
+            <div className="app">
+                <ThemeProvider theme={selectedTheme}>
+                    <GlobalStyles />
+                    <Switch>
+                        <Route exact path="/">
+                            <Example setTheme={setSelectedTheme} />
+                        </Route>
+                    </Switch>
+                </ThemeProvider>
+            </div>
+        </Router>
     );
 };
 
