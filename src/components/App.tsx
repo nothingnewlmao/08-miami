bf77a46fe9e8ccebeea6e1146acadbb4bb893f7d
Valentine <valentine.kirsanova@gmail.com>
2021-09-23 11:41:51 +0300

    deleted testing router links; moved router into App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { GlobalStyles } from '../../ui/global';
 import { themes } from '../../ui/themes';
 import { Example } from './Example/Example';
@@ -9,29 +9,18 @@ const App: FC = () => {
     const [selectedTheme, setSelectedTheme] = useState(themes.light);
 
     return (
-        <div className="app">
-            <ThemeProvider theme={selectedTheme}>
-                <GlobalStyles />
-                <ul>
-                    <li>
-                        <Link to="/">root</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">about</Link>
-                    </li>
-                </ul>
-                <Switch>
-                    <Route exact path="/">
-                        <Example setTheme={setSelectedTheme} />
-                    </Route>
-                    <Route path="/about">
-                        <div className="about">
-                            <h2>About</h2>
-                        </div>
-                    </Route>
-                </Switch>
-            </ThemeProvider>
-        </div>
+        <Router>
+            <div className="app">
+                <ThemeProvider theme={selectedTheme}>
+                    <GlobalStyles />
+                    <Switch>
+                        <Route exact path="/">
+                            <Example setTheme={setSelectedTheme} />
+                        </Route>
+                    </Switch>
+                </ThemeProvider>
+            </div>
+        </Router>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <App />,
     document.getElementById('root'),
 );
