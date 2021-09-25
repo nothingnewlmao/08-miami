d72af6b145fd5763843001a7e0b150d21477ae6c
a18245107 <AOSherstnev@sberbank.ru>
2021-09-25 13:11:55 +0300

    Обработка ошибок

---
+++ src/pages/ErrorMessage/styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { colors } from '../../../ui/colors';
+
+export const Wrapper = styled.div`
+    display: flex;
+    height: 100vh;
+    font-size: 70px;
+    font-weight: bold;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    background: ${colors.lightPrimary};
+`;
