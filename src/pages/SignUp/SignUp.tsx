88880bfa6e2fc6d5589ed194198bae00293f68cd
oduvankenobi <valentine.kirsanova@gmail.com>
2021-09-25 20:07:50 +0300

    eslint files fix

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -6,36 +6,13 @@ export const SignUp: FC = () => {
 
     return (
         <div className="sign-up">
-            <Input
-                ref={ref}
-                label="Почта"
-                name="ema!il"
-            />
-            <Input
-                label="Логин"
-                name="login"
-            />
-            <Input
-                label="Имя"
-                name="first_name"
-            />
-            <Input
-                label="Фамилия"
-                name="last_name"
-            />
-            <Input
-                label="Телефон"
-                name="phone"
-            />
-            <Input
-                label="Пароль"
-                type="password"
-                name="password"
-            />
-            <Input
-                label="Пароль (ещё раз)"
-                type="password"
-            />
+            <Input ref={ref} label="Почта" name="ema!il" />
+            <Input label="Логин" name="login" />
+            <Input label="Имя" name="first_name" />
+            <Input label="Фамилия" name="last_name" />
+            <Input label="Телефон" name="phone" />
+            <Input label="Пароль" type="password" name="password" />
+            <Input label="Пароль (ещё раз)" type="password" />
         </div>
     );
 };
