1474ac1172300a1a9176af838f188c5aa2ed70bb
oduvankenobi <valentine.kirsanova@gmail.com>
2021-09-26 13:32:11 +0300

    get url inside baseApi

--- src/api/BaseApi/index.ts
+++ src/api/BaseApi/index.ts
@@ -1,10 +1,12 @@
 export default class BaseApi {
+    static URL = 'ya-praktikum.tech/api/v2';
+
     public static get(path: string) {
-        return fetch(`${process.env.API_URL}/${path}`);
+        return fetch(`${this.URL}/${path}`);
     }
 
     public static post(path: string, body: FormData | string) {
-        return fetch(`${process.env.API_URL}/${path}`, {
+        return fetch(`${this.URL}/${path}`, {
             method: 'POST',
             body,
         });
