1e215bfe09d816a401be6d461653c74d44112f32
alexmented <sm_alex99@mail.ru>
2021-09-26 09:36:40 +0300

    Feature/miami 6 Обработка ошибок (#7)
    * leaderboard
    
    * leaderboard
    
    * add sort
    
    * add button
    
    * fix
    
    * Обработка ошибок
    
    Co-authored-by: a18245107 <AOSherstnev@sberbank.ru>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 import { GlobalStyles } from '../../ui/global';
 import { themes } from '../../ui/themes';
 import { Example } from './Example/Example';
@@ -10,22 +11,24 @@ const App: FC = () => {
     const [selectedTheme, setSelectedTheme] = useState(themes.light);
 
     return (
-        <Router>
-            <div className="app">
-                <ThemeProvider theme={selectedTheme}>
-                    <GlobalStyles />
-                    <Switch>
-                        <Route exact path="/">
-                            <Example setTheme={setSelectedTheme} />
-                        </Route>
-                        <Route path="/leaderboard">
-                            <Leaderboard />
-                        </Route>
-                        <Redirect to="/" />
-                    </Switch>
-                </ThemeProvider>
-            </div>
-        </Router>
+        <ErrorBoundary>
+            <Router>
+                <div className="app">
+                    <ThemeProvider theme={selectedTheme}>
+                        <GlobalStyles />
+                        <Switch>
+                            <Route exact path="/">
+                                <Example setTheme={setSelectedTheme} />
+                            </Route>
+                            <Route path="/leaderboard">
+                                <Leaderboard />
+                            </Route>
+                            <Redirect to="/" />
+                        </Switch>
+                    </ThemeProvider>
+                </div>
+            </Router>
+        </ErrorBoundary>
     );
 };
 
