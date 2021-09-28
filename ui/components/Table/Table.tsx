c59d399e7b0d0aa02321400c31e6e746ead8c350
a18245107 <AOSherstnev@sberbank.ru>
2021-09-28 13:15:45 +0300

    fix alias

--- ui/components/Table/Table.tsx
+++ ui/components/Table/Table.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import * as Styled from './styled';
 
-export const Table = React.forwardRef<HTMLTableElement, React.TableHTMLAttributes<HTMLElement>>(
-    ({ ...props }, ref) => {
-        const { children, ...rest } = props;
+export const Table = React.forwardRef<
+    HTMLTableElement,
+    React.TableHTMLAttributes<HTMLElement>
+>(({ ...props }, ref) => {
+    const { children, ...rest } = props;
 
-        return (
-            <Styled.Table ref={ref as React.MutableRefObject<HTMLTableElement>} {...rest}>
-                {children}
-            </Styled.Table>
-        );
-    },
-);
+    return (
+        <Styled.Table
+            ref={ref as React.MutableRefObject<HTMLTableElement>}
+            {...rest}
+        >
+            {children}
+        </Styled.Table>
+    );
+});
