b67e10d22f930202674654e06311d24eecb40d3b
Valentine <valentine.kirsanova@gmail.com>
2021-09-28 19:07:01 +0300

    fixed global.ts styles

--- ui/global.ts
+++ ui/global.ts
@@ -3,23 +3,20 @@ import { colors } from 'ui/colors';
 import { ITheme } from './themes';
 
 export const GlobalStyles = createGlobalStyle<ITheme>`
-  body {
-    padding: 0;
-    color: ${({ theme }) => theme.colors.text};
-    background: ${({ theme }) => theme.colors.body};
-    margin: 0;
-    font-family: 'Roboto', sans-serif;
-    
+
     html, #root, .app, body {
         height: 100%;
     }
-      
-      .-error {
-          color: ${colors.danger};
-      }
-  }
-
-  html, #root, .app, body {
-    height: 100%;
-}
+    
+    body {
+        padding: 0;
+        color: ${({ theme }) => theme.colors.text};
+        background: ${({ theme }) => theme.colors.body};
+        margin: 0;
+        font-family: 'Roboto', sans-serif;
+        
+        .-error {
+            color: ${colors.danger};
+        }
+    }
 `;
