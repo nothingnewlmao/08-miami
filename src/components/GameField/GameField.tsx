8364c3f537bcfa946fb62db232910cbfa94705f8
Москварцев Семён <semen.moskvarcev@gmail.com>
2021-09-28 12:17:07 +0300

    Feature/Miami 12: Добавить отрисовку портала выхода с уровня и прокинуть game-over-callback (#11)
    * Feature/Miami-7: Добавить страницу загрузки
    
    * Feature/Miami-12: Добавить отрисовку game-over-портала и прокинул game-over-callback
    
    * Feature/Miami-12: Добавить redirect to leaderboard
    
    Co-authored-by: Semen Moskvartsev <smoskvartsev@yandex-team.ru>

--- src/components/GameField/GameField.tsx
+++ src/components/GameField/GameField.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import Game from 'services/Game/Game';
+import { Game } from 'services/Game';
+import { useHistory } from 'react-router-dom';
 
 export interface IGameFieldProps {
     fieldHeight?: number;
@@ -11,18 +12,34 @@ export const GameField: React.FC<IGameFieldProps> = ({
     fieldWidth = window.innerWidth,
 }: IGameFieldProps) => {
     const canvasRef = React.useRef<HTMLCanvasElement | null>(null);
+    const history = useHistory();
 
     React.useEffect(() => {
         const canvasElem = canvasRef.current;
 
         if (canvasElem) {
+            const gameOverCallback = () => history.push('/leaderboard');
+
             const game = new Game({
                 canvasRef: canvasElem,
-                initX: 100,
-                initY: 100,
+                initPoint: {
+                    x: 100,
+                    y: 100,
+                },
+                gameOverPoint: {
+                    x: fieldWidth - 50,
+                    y: fieldHeight - 50,
+                },
+                gameOverCallback,
             });
             game.start();
+
+            return () => {
+                game.unsubscribeKeysCallback();
+            };
         }
+
+        return () => {};
     }, [canvasRef]);
 
     return <canvas ref={canvasRef} height={fieldHeight} width={fieldWidth} />;
