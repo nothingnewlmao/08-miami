5dd16d61e2096409da9e3dea7676f0aeb4f5067b
Valentine <valentine.kirsanova@gmail.com>
2021-09-30 12:35:54 +0300

    Revert "installed azios; fixed npm packages versions"
    This reverts commit f0665a1d23a9df71e46d546087355a5826e8d4c0.

--- src/pages/Leaderboard/styled.ts
+++ src/pages/Leaderboard/styled.ts
@@ -4,21 +4,13 @@ import { Button } from 'uicomponents/Button';
 import { colors } from 'ui/colors';
 import { Table } from 'uicomponents/Table/Table';
 
-export const Wrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    text-align: center;
-`;
-
 export const Title = styled.h2<ITheme>`
     margin-bottom: 10px;
     color: ${({ theme }) => theme.colors.text};
 `;
 
 export const LeaderTable = styled(Table)`
-    border-spacing: 0 10px;
+    border-spacing: 0 8px;
 `;
 
 export const BackButton = styled(Button)`
