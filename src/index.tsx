62cc48ddb09214a9c04b2a13cbc9e4a31c4ced26
oduvankenobi <43110071+oduvankenobi@users.noreply.github.com>
2021-10-10 13:11:01 +0300

    Feature/miami-24 Добавление redux-toolkit  (#19)
    * added .vscode to .gitignore
    
    * fixed npm packages versions
    
    * added redux toolkit
    
    * added resolving paths for store
    
    * added store
    
    * installed reselect
    
    * deleted extra files
    
    * deleted store from aliases
    
    * store to service in tsconfig.json
    
    * fixed alias
    
    * fixed alias AGAIN

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,12 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from 'pages/App';
+import { store } from 'store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
