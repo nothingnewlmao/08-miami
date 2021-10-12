881ecbc1f2b8c95bf307012d78a909e4e0dcbbf8
Valentine <valentine.kirsanova@gmail.com>
2021-10-12 14:36:54 +0300

    trying store slice

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -3,13 +3,19 @@ import * as Yup from 'yup';
 import { signIn } from 'api/axios';
 import { FormikValues } from 'formik';
 import { RouteComponentProps } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { resetPending, setPending } from 'store/userProfile/slice';
 
 import { FormWithRouter } from 'uicomponents/Form';
 
-const validationSchema = Yup.object().shape({
-    login: Yup.string().required('Введите логин'),
-    password: Yup.string().required('Введите пароль'),
-});
+const validationSchema = Yup.object()
+    .shape({
+        login: Yup.string()
+            .required('Введите логин'),
+        password: Yup.string()
+            .required('Введите пароль'),
+    });
 
 const initialValues = {
     login: '',
@@ -29,26 +35,30 @@ const fields = [
 ];
 
 export const SignInWithData = () => {
+    const dispatch = useDispatch();
+
     const handleSubmit = (
         values: FormikValues,
         history: RouteComponentProps['history'],
     ) => {
+        dispatch(setPending());
         signIn(values)
             .then(() => {
                 history.push('/');
             })
-            .catch((err) => console.log(err));
+            .catch((err) => console.log(err))
+            .finally(() => {
+                dispatch(resetPending());
+            });
     };
 
     return (
-        <div>
-            <FormWithRouter
-                validationSchema={validationSchema}
-                handleSubmit={handleSubmit}
-                title="Вход"
-                fields={fields}
-                initialValues={initialValues}
-            />
-        </div>
+        <FormWithRouter
+            validationSchema={validationSchema}
+            handleSubmit={handleSubmit}
+            title="Вход"
+            fields={fields}
+            initialValues={initialValues}
+        />
     );
 };
