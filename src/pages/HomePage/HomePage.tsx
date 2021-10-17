2e34946c6ec66887978ee279fd98fb888b5b4c33
Valentine <valentine.kirsanova@gmail.com>
2021-10-17 10:45:57 +0300

    refactored logout callback for Homepage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,32 +1,32 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
 import { Link } from 'react-router-dom';
-import { logOut } from 'api/axios';
+import { logOut } from 'api/authApi';
 
 import * as Styled from './styled';
 
-const handleLogOutClick = () => {
-    logOut();
-};
+export const HomePage: FC = () => {
+    const handleLogOutClick = useCallback(logOut, []);
 
-export const HomePage: FC = () => (
-    <Styled.Wrapper>
-        <Styled.Container>
-            <Styled.MainTitle>Bounce: Returning</Styled.MainTitle>
-            <Styled.MenuButton>
-                <Link to="/loading">Играть</Link>
-            </Styled.MenuButton>
-            <Styled.MenuButton>
-                <Link to="/user"> Игрок </Link>
-            </Styled.MenuButton>
-            <Styled.MenuButton>
-                <Link to="/leaderboard">Таблица рекордов</Link>
-            </Styled.MenuButton>
-            <Styled.MenuButton>
-                <Link to="/forum">Форум</Link>
-            </Styled.MenuButton>
-            <Styled.MenuButton onClick={() => handleLogOutClick()}>
-                <Link to="/sign-in">Выйти</Link>
-            </Styled.MenuButton>
-        </Styled.Container>
-    </Styled.Wrapper>
-);
+    return (
+        <Styled.Wrapper>
+            <Styled.Container>
+                <Styled.MainTitle>Bounce: Returning</Styled.MainTitle>
+                <Styled.MenuButton>
+                    <Link to="/loading">Играть</Link>
+                </Styled.MenuButton>
+                <Styled.MenuButton>
+                    <Link to="/user"> Игрок </Link>
+                </Styled.MenuButton>
+                <Styled.MenuButton>
+                    <Link to="/leaderboard">Таблица рекордов</Link>
+                </Styled.MenuButton>
+                <Styled.MenuButton>
+                    <Link to="/forum">Форум</Link>
+                </Styled.MenuButton>
+                <Styled.MenuButton onClick={handleLogOutClick}>
+                    <Link to="/sign-in">Выйти</Link>
+                </Styled.MenuButton>
+            </Styled.Container>
+        </Styled.Wrapper>
+    );
+};
