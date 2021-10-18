8b52ff2e961355d451c1f6e5df475cec6e50a5bd
Valentine <valentine.kirsanova@gmail.com>
2021-10-18 18:27:17 +0300

    redirect user to root after successful sign in

--- src/store/userProfile/sagas.ts
+++ src/store/userProfile/sagas.ts
@@ -4,6 +4,8 @@ import { signIn } from 'api/authApi';
 import { dataFailed, dataFetching, dataLoaded } from 'store/userProfile/slice';
 import ActionTypes from 'store/userProfile/actionTypes';
 
+import history from 'utils/history';
+
 function* signInRequest(action: any) {
     yield put(dataFetching());
     const { payload } = action;
@@ -12,11 +14,12 @@ function* signInRequest(action: any) {
         yield call(signIn, payload);
 
         const { login } = payload;
-        
+
         yield put(dataLoaded({ login }));
+        yield call([history, history.push], '/');
     } catch (e: any) {
         const { reason } = e.response.data;
-        
+
         yield put(dataFailed(reason));
     }
 }
