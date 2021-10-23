95e993601beba8476f51f0771076990d31eafee2
Valentine <valentine.kirsanova@gmail.com>
2021-10-23 11:53:03 +0300

    added snapshot for Form.tsx

--- ui/components/Form/__tests__/Form.spec.tsx
+++ ui/components/Form/__tests__/Form.spec.tsx
@@ -1,46 +1,41 @@
 import 'jsdom-global/register';
 import React from 'react';
-import {
-    render,
-    cleanup,
-    fireEvent,
-} from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
+import renderer from 'react-test-renderer';
 
 import { themes } from 'ui/themes';
 import { InnerForm as Form } from 'ui/components/Form';
 
 const { light } = themes;
 
-const createRTLInstance = () => {
-    const initialValues = {
-        login: '',
-        password: '',
-    };
-
-    const fields = [
-        {
-            label: 'Логин',
-            name: 'login',
-        },
-        {
-            label: 'Пароль',
-            name: 'password',
-            type: 'password',
-        },
-    ];
-
-    return render(
-        <ThemeProvider theme={light}>
-            <Form
-                title="Вход"
-                handleSubmit={() => ''}
-                fields={fields}
-                initialValues={initialValues}
-            />
-        </ThemeProvider>,
-    );
+const initialValues = {
+    login: '',
+    password: '',
 };
+
+const fields = [
+    {
+        label: 'Логин',
+        name: 'login',
+    },
+    {
+        label: 'Пароль',
+        name: 'password',
+        type: 'password',
+    },
+];
+
+const template = () => (
+    <ThemeProvider theme={light}>
+        <Form
+            title="Title"
+            handleSubmit={() => ''}
+            fields={fields}
+            initialValues={initialValues}
+        />
+    </ThemeProvider>
+);
 
 const callback = jest.fn();
 
@@ -48,8 +43,17 @@ afterEach(() => {
     cleanup();
 });
 
-describe('Form tests', () => {
+describe('Form', () => {
     it('Should ???', async () => {
+        /**
+         * тестирование отправки данных по нажатию кнопки нецелесообразно,
+         * так как это, по сути, тестирование сайд-эффекта саги
+         *
+         * тестирование отображения ошибок пустого поля в форме после нажатия на кнопку отправить
+         * так же не целесообразно, так как это тестирование рендера ошибки у инпута
+         */
+        const createRTLInstance = () => render(template());
+
         const { getByText, getByLabelText } = createRTLInstance();
         const input = getByLabelText('Логин');
         const submitBtn = getByText('Присоединиться');
@@ -59,5 +63,11 @@ describe('Form tests', () => {
         fireEvent.click(submitBtn);
 
         console.log(input);
+    });
+
+    it('Is equal to snapshot', () => {
+        const tree = renderer.create(template()).toJSON();
+
+        expect(tree).toMatchSnapshot();
     });
 });
