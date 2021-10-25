9f4e9f53335a636c7ed581147d5ff2157d488611
Valentine <valentine.kirsanova@gmail.com>
2021-10-25 14:43:54 +0300

    returned Input label back

--- ui/components/Input/Input.tsx
+++ ui/components/Input/Input.tsx
@@ -63,19 +63,16 @@ export const StyledError = styled.div`
 
 export const Input = React.forwardRef<HTMLInputElement, TStyledInput>(
     ({ ...props }, ref) => {
-        const {
-            children, label = '', name, ...rest
-        } = props;
+        const { children, label = '', ...rest } = props;
         return (
             <StyledInputWrapper>
                 <StyledInput
                     ref={ref as React.MutableRefObject<HTMLInputElement>}
-                    id={`form-input-${name}`}
                     {...rest}
                 >
                     {children}
                 </StyledInput>
-                <label htmlFor={`form-input-${name}`}>{label}</label>
+                <label htmlFor={rest.name}>{label}</label>
                 <StyledError>{rest.errorText}</StyledError>
             </StyledInputWrapper>
         );
