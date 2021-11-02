3660afcce058d666fdb7f644a7b82c98b16d92a3
Valentine <valentine.kirsanova@gmail.com>
2021-11-02 18:04:44 +0300

    added webpack-dotenv and redirect_uri to .env

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     mode: 'development',
@@ -51,6 +52,7 @@ module.exports = {
         },
     },
     plugins: [
+        new Dotenv(),
         new HtmlWebpackPlugin({
             template: './www/index.html',
             title: 'Bounce Game :)',
