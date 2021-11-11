fbe4f87bfd7c2a38e89b19c8eb1f590b43a2787d
Valentine <valentine.kirsanova@gmail.com>
2021-11-11 18:13:32 +0300

    deleted extra types

--- src/types/TPrivateRouteProps.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { RouteProps } from 'react-router-dom';
-
-type TPrivateRouteProps = {
-    path: RouteProps['path'];
-    component: React.ElementType;
-};
-
-export default TPrivateRouteProps;
