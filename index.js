5a22508be39bdde42ae22127ec673e06c08c01ed
Valentine <valentine.kirsanova@gmail.com>
2021-11-20 11:14:19 +0300

    connect postgres

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const { app } = require('./dist/server.js');
 
 const port = process.env.PORT || 3000;
 
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'my-db-name',
+    password: 'newPassword',
+    port: 5432,
+});
+
+client.connect();
+
+client
+    .query('SELECT NOW()')
+    .then((res) => {
+        console.log(res.rows);
+        client.end();
+    })
+    .catch((error) => {
+        console.log(`Postgres error: ${error}`);
+    });
+
 app.listen(port, () => {
     console.log('Application is started on localhost: ', port);
 });
