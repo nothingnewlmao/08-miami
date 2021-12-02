e989c40a6f7c2b3bda43b24b0356a70c197b2dbd
Valentine <valentine.kirsanova@gmail.com>
2021-12-03 00:52:04 +0300

    inited models

--- src/initSequelize.ts
+++ src/initSequelize.ts
@@ -1,6 +1,8 @@
 import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
 
 import { userModel } from 'models/User/User';
+import { messageModel } from 'models/Message/Message';
+import { topicModel } from 'models/Topic/Topic';
 
 const sequelizeOptions: SequelizeOptions = {
     host: process.env.POSTGRES_HOST,
@@ -13,7 +15,17 @@ const sequelizeOptions: SequelizeOptions = {
 
 export const sequelize = new Sequelize(sequelizeOptions);
 
+export const Topic = sequelize.define('Topic', topicModel, {});
+
 export const User = sequelize.define('User', userModel, {});
+
+export const Message = sequelize.define('Message', messageModel, {});
+
+User.hasMany(Message);
+Message.belongsTo(User);
+
+Topic.hasMany(Message);
+Message.belongsTo(Topic);
 
 export async function dbConnect() {
     try {
